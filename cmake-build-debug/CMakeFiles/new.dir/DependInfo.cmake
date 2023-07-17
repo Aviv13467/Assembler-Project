237851656836091c991db90704c5d618790756a1
aviv <aviv13467@gmail.com>
2023-07-17 17:29:04 +0300

    Update 17/07/23 17:27 Started working on first_pass command parsing

---
+++ cmake-build-debug/CMakeFiles/new.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aviv/CLionProjects/Maman14 copy/first_pass.c" "CMakeFiles/new.dir/first_pass.c.o" "gcc" "CMakeFiles/new.dir/first_pass.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/hashtable.c" "CMakeFiles/new.dir/hashtable.c.o" "gcc" "CMakeFiles/new.dir/hashtable.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/main.c" "CMakeFiles/new.dir/main.c.o" "gcc" "CMakeFiles/new.dir/main.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/memory.c" "CMakeFiles/new.dir/memory.c.o" "gcc" "CMakeFiles/new.dir/memory.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/parse_line.c" "CMakeFiles/new.dir/parse_line.c.o" "gcc" "CMakeFiles/new.dir/parse_line.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/preprocessor.c" "CMakeFiles/new.dir/preprocessor.c.o" "gcc" "CMakeFiles/new.dir/preprocessor.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/symbol_table.c" "CMakeFiles/new.dir/symbol_table.c.o" "gcc" "CMakeFiles/new.dir/symbol_table.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14 copy/utils.c" "CMakeFiles/new.dir/utils.c.o" "gcc" "CMakeFiles/new.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/new.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new.dir/first_pass.c.o"
+  "CMakeFiles/new.dir/first_pass.c.o.d"
+  "CMakeFiles/new.dir/hashtable.c.o"
+  "CMakeFiles/new.dir/hashtable.c.o.d"
+  "CMakeFiles/new.dir/main.c.o"
+  "CMakeFiles/new.dir/main.c.o.d"
+  "CMakeFiles/new.dir/memory.c.o"
+  "CMakeFiles/new.dir/memory.c.o.d"
+  "CMakeFiles/new.dir/parse_line.c.o"
+  "CMakeFiles/new.dir/parse_line.c.o.d"
+  "CMakeFiles/new.dir/preprocessor.c.o"
+  "CMakeFiles/new.dir/preprocessor.c.o.d"
+  "CMakeFiles/new.dir/symbol_table.c.o"
+  "CMakeFiles/new.dir/symbol_table.c.o.d"
+  "CMakeFiles/new.dir/utils.c.o"
+  "CMakeFiles/new.dir/utils.c.o.d"
+  "new"
+  "new.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
