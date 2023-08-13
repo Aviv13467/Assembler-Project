e8e05f297b986bda878e7af3310336ccb65e936d
aviv <aviv13467@gmail.com>
2023-08-13 23:00:22 +0300

    Update 13/08/23 23:00 PM Fixed all (Run on Ubuntu 22.4) warnings, compiles OK.

---
+++ cmake-build-debug/CMakeFiles/Maman14.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aviv/CLionProjects/Maman14/LinkedList.c" "CMakeFiles/Maman14.dir/LinkedList.c.o" "gcc" "CMakeFiles/Maman14.dir/LinkedList.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/entry_table.c" "CMakeFiles/Maman14.dir/entry_table.c.o" "gcc" "CMakeFiles/Maman14.dir/entry_table.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/extern_table.c" "CMakeFiles/Maman14.dir/extern_table.c.o" "gcc" "CMakeFiles/Maman14.dir/extern_table.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/first_pass.c" "CMakeFiles/Maman14.dir/first_pass.c.o" "gcc" "CMakeFiles/Maman14.dir/first_pass.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/hashtable.c" "CMakeFiles/Maman14.dir/hashtable.c.o" "gcc" "CMakeFiles/Maman14.dir/hashtable.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/main.c" "CMakeFiles/Maman14.dir/main.c.o" "gcc" "CMakeFiles/Maman14.dir/main.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/memory.c" "CMakeFiles/Maman14.dir/memory.c.o" "gcc" "CMakeFiles/Maman14.dir/memory.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/preprocessor.c" "CMakeFiles/Maman14.dir/preprocessor.c.o" "gcc" "CMakeFiles/Maman14.dir/preprocessor.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/second_pass.c" "CMakeFiles/Maman14.dir/second_pass.c.o" "gcc" "CMakeFiles/Maman14.dir/second_pass.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/symbol_table.c" "CMakeFiles/Maman14.dir/symbol_table.c.o" "gcc" "CMakeFiles/Maman14.dir/symbol_table.c.o.d"
+  "/Users/aviv/CLionProjects/Maman14/utils.c" "CMakeFiles/Maman14.dir/utils.c.o" "gcc" "CMakeFiles/Maman14.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Maman14.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maman14.dir/LinkedList.c.o"
+  "CMakeFiles/Maman14.dir/LinkedList.c.o.d"
+  "CMakeFiles/Maman14.dir/entry_table.c.o"
+  "CMakeFiles/Maman14.dir/entry_table.c.o.d"
+  "CMakeFiles/Maman14.dir/extern_table.c.o"
+  "CMakeFiles/Maman14.dir/extern_table.c.o.d"
+  "CMakeFiles/Maman14.dir/first_pass.c.o"
+  "CMakeFiles/Maman14.dir/first_pass.c.o.d"
+  "CMakeFiles/Maman14.dir/hashtable.c.o"
+  "CMakeFiles/Maman14.dir/hashtable.c.o.d"
+  "CMakeFiles/Maman14.dir/main.c.o"
+  "CMakeFiles/Maman14.dir/main.c.o.d"
+  "CMakeFiles/Maman14.dir/memory.c.o"
+  "CMakeFiles/Maman14.dir/memory.c.o.d"
+  "CMakeFiles/Maman14.dir/preprocessor.c.o"
+  "CMakeFiles/Maman14.dir/preprocessor.c.o.d"
+  "CMakeFiles/Maman14.dir/second_pass.c.o"
+  "CMakeFiles/Maman14.dir/second_pass.c.o.d"
+  "CMakeFiles/Maman14.dir/symbol_table.c.o"
+  "CMakeFiles/Maman14.dir/symbol_table.c.o.d"
+  "CMakeFiles/Maman14.dir/utils.c.o"
+  "CMakeFiles/Maman14.dir/utils.c.o.d"
+  "Maman14"
+  "Maman14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Maman14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
