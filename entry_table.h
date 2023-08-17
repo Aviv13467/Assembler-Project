1f3d8f05b5d39a033f86cdf16ba4f84928dae3bf
aviv <aviv13467@gmail.com>
2023-08-17 11:28:26 +0300

    Update 17/08/23 11:28 AM

--- entry_table.h
+++ entry_table.h
@@ -12,11 +12,30 @@ typedef struct entry_table{
     int label_pos;
     struct entry_table *next;
 }entry_table;
+
+/*
+ * create_entry functions creates a new entry table based on a LinkedList.
+ */
 entry_table *create_entry(void);
+/*
+ * set_entry function sets values to the given node.
+ */
 void set_entry(entry_table *sym,char *name,int pos);
+/*
+ * add_entry function adds a new node to the entry table.
+ */
 entry_table* add_entry(entry_table **head, char *label,int pos);
+/*
+ * fprint_entry_table function writes all the entries to the given file.
+ */
 void fprint_entry_table(entry_table *head,FILE *ofp);
+/*
+ * isEmpty_entry function checks whether the table is empty.
+ */
 int isEmpty_entry(entry_table *head);
+/*
+ * free_entry function deletes all nodes in the table and frees the allocated memory.
+ */
 void free_entry(entry_table *head);
 
 #endif
